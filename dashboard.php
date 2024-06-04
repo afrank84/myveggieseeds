76665907edce7bd6d3a9b594f42428802651e9b9
Anthony Frank <afrank84@yahoo.com>
2024-06-04 11:00:12 -0400

    dashboard.php - got lgin working again.

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
