aedd9de87261ed3d02d7fa19498a6a72b1e83337
Anthony Frank <afrank84@yahoo.com>
2024-06-04 16:22:29 -0400

    search_plants_form.php - redirect to login_page.php if not signed in.

--- search_plants_form.php
+++ search_plants_form.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login_page.php");
+    exit();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
